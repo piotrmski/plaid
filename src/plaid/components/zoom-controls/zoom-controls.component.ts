f87a83b78ebb00db2479300ffbf50f330abb4f94
Piotr Marczyński <piotrmski@gmail.com>
2020-02-10 21:21:19 +0100

    Zoom control buttons

---
+++ src/plaid/components/zoom-controls/zoom-controls.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'plaid-zoom-controls',
+  templateUrl: './zoom-controls.component.html'
+})
+export class ZoomControlsComponent {
+}
