b2236879f514b71735eef32053882ef51df23555
Piotr Marczyński <piotrmski@gmail.com>
2020-03-04 00:13:29 +0100

    Implemented work log panel stretching, improved dragging

--- src/plaid/components/worklog-editor/worklog-editor.component.ts
+++ src/plaid/components/worklog-editor/worklog-editor.component.ts
@@ -16,13 +16,17 @@ import {Worklog} from '../../models/worklog';
   styleUrls: ['./worklog-editor.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WorklogEditorComponent implements OnInit {
+export class WorklogEditorComponent {
+  static readonly GRID_OFFSET_TOP = 71; // top bar height + grid header height
+  static readonly GRID_OFFSET_LEFT = 30; // hour labels width
+  static readonly STRETCH_HANDLE_OFFSET_TOP = 4; // offset between top of a stretching handle and edge of the panel
+
   _pixelsPerMinute: number;
   _dateRange: DateRange;
   _worklog: Worklog;
   start: Date;
-  startDate: Date;
-  durationSeconds: number;
+  date: Date;
+  durationMinutes: number;
   panelOffsetTop: number;
   panelHeight: number;
   panelOffsetLeft: number;
@@ -30,21 +34,21 @@ export class WorklogEditorComponent implements OnInit {
   editedPanelInRange: boolean;
   panelHue: number;
   dragging = false;
-  dragStartXOffset: number;
-  dragStartYOffset: number;
-  dragEventListener: (e: MouseEvent) => void;
-  mouseupEventListener: () => void;
+  stretching = false;
+  mouseEventXOffset: number;
+  mouseEventYOffset: number;
+  mousemoveEventListener: (e: MouseEvent) => void;
 
   @Output()
   cancelEdit = new EventEmitter<void>();
 
   @Input()
-  scrollableAncestor: HTMLDivElement;
+  gridElement: HTMLDivElement;
 
   @Input()
   set pixelsPerMinute(value: number) {
-    if (this.dragStartYOffset != null) {
-      this.dragStartYOffset *= value / this._pixelsPerMinute;
+    if (this.mouseEventYOffset != null) {
+      this.mouseEventYOffset *= value / this._pixelsPerMinute;
     }
     this._pixelsPerMinute = value;
     if (this.worklog && this.editedPanelInRange) {
@@ -59,7 +63,7 @@ export class WorklogEditorComponent implements OnInit {
   set dateRange(range: DateRange) {
     this._dateRange = range;
     if (this.worklog) {
-      this.editedPanelInRange = this.startDate >= range.start && this.startDate <= range.end;
+      this.editedPanelInRange = this.date >= range.start && this.date <= range.end;
       if (this.editedPanelInRange) {
         this.computeSizeAndOffset();
       }
@@ -75,10 +79,10 @@ export class WorklogEditorComponent implements OnInit {
     if (worklog) {
       this.start = new Date(worklog.started);
       this.start.setSeconds(0, 0);
-      this.startDate = new Date(this.start);
-      this.startDate.setHours(0, 0, 0, 0);
-      this.durationSeconds = worklog.timeSpentSeconds;
-      this.editedPanelInRange = this.startDate >= this.dateRange.start && this.startDate <= this.dateRange.end;
+      this.date = new Date(this.start);
+      this.date.setHours(0, 0, 0, 0);
+      this.durationMinutes = Math.round(worklog.timeSpentSeconds / 60);
+      this.editedPanelInRange = this.date >= this.dateRange.start && this.date <= this.dateRange.end;
       this.panelHue = Math.round((Number(this.worklog.issue.fields.parent
         ? this.worklog.issue.fields.parent.id
         : this.worklog.issue.id) * 360 / 1.61803)) % 360;
@@ -94,89 +98,146 @@ export class WorklogEditorComponent implements OnInit {
   constructor(private cdr: ChangeDetectorRef) {
   }
 
-  ngOnInit(): void {
-    this.mouseupEventListener = () => this.dragEnd();
-    // Singleton component, no need to unbind.
-    document.addEventListener('mouseup', () => this.dragEnd());
-  }
-
   computeSizeAndOffset(): void {
     this.panelOffsetTop = (this.start.getHours() * 60 + this.start.getMinutes()) * this.pixelsPerMinute;
     this.panelHeight = Math.min(
-      this.durationSeconds / 60 * this.pixelsPerMinute,
+      this.durationMinutes * this.pixelsPerMinute,
       1440 * this.pixelsPerMinute - this.panelOffsetTop
     );
     this.panelWidth = 1 / (Math.round((this.dateRange.end.getTime() - this.dateRange.start.getTime()) / 86400000) + 1);
-    this.panelOffsetLeft = this.panelWidth * Math.round((this.startDate.getTime() - this.dateRange.start.getTime()) / 86400000);
+    this.panelOffsetLeft = this.panelWidth * Math.round((this.date.getTime() - this.dateRange.start.getTime()) / 86400000);
   }
 
   dragStart(event: MouseEvent): void {
     this.dragging = true;
-    this.dragStartXOffset = this.scrollableAncestor.scrollLeft + event.clientX;
-    this.dragStartYOffset = this.scrollableAncestor.scrollTop + event.clientY;
-    this.dragEventListener = e => this.handleDragEvent(e);
-    document.addEventListener('mousemove', this.dragEventListener);
+    this.mouseEventXOffset = event.offsetX;
+    this.mouseEventYOffset = event.offsetY;
+    this.mousemoveEventListener = e => this.handleDragEvent(e);
+    document.addEventListener('mousemove', this.mousemoveEventListener);
+    document.addEventListener('mouseup', () => this.dragEnd(), {once: true});
   }
 
   dragEnd(): void {
     this.dragging = false;
     this.cdr.detectChanges();
-    if (this.dragEventListener) {
-      document.removeEventListener('mousemove', this.dragEventListener);
+    if (this.mousemoveEventListener) {
+      document.removeEventListener('mousemove', this.mousemoveEventListener);
     }
   }
 
   handleDragEvent(event: MouseEvent): void {
-    // How many minutes to snap to
-    let snapTo = 5;
-    if (event.altKey && !event.ctrlKey && !event.shiftKey) {
-      snapTo = 1;
-    } else if (!event.altKey && event.ctrlKey && !event.shiftKey) {
-      snapTo = 60;
-    } else if (!event.altKey && !event.ctrlKey && event.shiftKey) {
-      snapTo = 15;
-    }
-
     // Handle dragging vertically
-    const dragEndYOffset: number = this.scrollableAncestor.scrollTop + event.clientY;
-    const minutesOffset: number = Math.round((dragEndYOffset - this.dragStartYOffset) / this.pixelsPerMinute);
     const oldStartTimeMinutes: number = this.start.getHours() * 60 + this.start.getMinutes();
-    const newStartTimeMinutes: number = Math.round((oldStartTimeMinutes + minutesOffset) / snapTo) * snapTo;
+    let newStartTimeMinutes: number = this.getPointerTopOffsetMinutes(event, this.getSnapTo(event));
     if (oldStartTimeMinutes !== newStartTimeMinutes) {
       if (newStartTimeMinutes < 0) { // Prevent dragging above the grid
-        this.start.setHours(0, 0, 0, 0);
-      } else if (newStartTimeMinutes + this.worklog.timeSpentSeconds / 60 > 1440) { // Prevent dragging below the grid
-        this.start.setHours(0, 1440 - this.worklog.timeSpentSeconds / 60, 0, 0);
-      } else {
-        this.start.setHours(0, newStartTimeMinutes, 0, 0);
-        this.dragStartYOffset += (newStartTimeMinutes - oldStartTimeMinutes) * this.pixelsPerMinute;
+        newStartTimeMinutes = 0;
+      } else if (newStartTimeMinutes + this.durationMinutes > 1440) { // Prevent dragging below the grid
+        newStartTimeMinutes = 1440 - this.durationMinutes;
       }
+      this.start.setHours(0, newStartTimeMinutes, 0, 0);
     }
 
     // Handle dragging horizontally
-    const pixelsPerDay: number = (this.scrollableAncestor.scrollWidth - 30) * this.panelWidth;
-    const dragEndXOffset: number = this.scrollableAncestor.scrollLeft + event.clientX;
-    const daysOffset: number = Math.round((dragEndXOffset - this.dragStartXOffset) / pixelsPerDay);
-    const oldDate: Date = new Date(this.startDate);
-    const newDate: Date = new Date(this.startDate);
-    newDate.setDate(newDate.getDate() + daysOffset);
+    const pixelsPerDay: number = (this.gridElement.scrollWidth - WorklogEditorComponent.GRID_OFFSET_LEFT) * this.panelWidth;
+    const oldDate: Date = new Date(this.date);
+    const newDate: Date = new Date(this.dateRange.start);
+    newDate.setDate(newDate.getDate() + Math.round((event.clientX + this.gridElement.scrollLeft -
+      WorklogEditorComponent.GRID_OFFSET_LEFT - this.mouseEventXOffset) / pixelsPerDay));
     if (oldDate.getTime() !== newDate.getTime()) {
       if (newDate < this.dateRange.start) { // Prevent dragging to before the visible date range
-        this.start.setFullYear(this.dateRange.start.getFullYear(), this.dateRange.start.getMonth(), this.dateRange.start.getDate());
-        this.startDate.setFullYear(this.dateRange.start.getFullYear(), this.dateRange.start.getMonth(), this.dateRange.start.getDate());
+        this.date = new Date(this.dateRange.start);
       } else if (newDate > this.dateRange.end) { // Prevent dragging to after the visible date range
-        this.start.setFullYear(this.dateRange.end.getFullYear(), this.dateRange.end.getMonth(), this.dateRange.end.getDate());
-        this.startDate.setFullYear(this.dateRange.end.getFullYear(), this.dateRange.end.getMonth(), this.dateRange.end.getDate());
+        this.date = new Date(this.dateRange.end);
       } else {
-        this.start.setDate(this.start.getDate() + daysOffset);
-        this.startDate.setDate(this.startDate.getDate() + daysOffset);
-        this.dragStartXOffset += (newDate.getTime() - oldDate.getTime()) / 86400000 * pixelsPerDay;
+        this.date = newDate;
       }
+      this.start.setFullYear(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
     }
 
     if (oldStartTimeMinutes !== newStartTimeMinutes || oldDate.getTime() !== newDate.getTime()) {
       this.computeSizeAndOffset();
       this.cdr.detectChanges();
     }
+  }
+
+  stretchTopStart(event: MouseEvent): void {
+    this.stretching = true;
+    this.mouseEventYOffset = event.offsetY - WorklogEditorComponent.STRETCH_HANDLE_OFFSET_TOP;
+    this.mousemoveEventListener = e => this.handleStretchTopEvent(e);
+    document.addEventListener('mousemove', this.mousemoveEventListener);
+    document.addEventListener('mouseup', () => this.stretchEnd(), {once: true});
+  }
+
+  stretchBottomStart(event: MouseEvent): void {
+    this.stretching = true;
+    this.mouseEventYOffset = event.offsetY - WorklogEditorComponent.STRETCH_HANDLE_OFFSET_TOP;
+    this.mousemoveEventListener = e => this.handleStretchBottomEvent(e);
+    document.addEventListener('mousemove', this.mousemoveEventListener);
+    document.addEventListener('mouseup', () => this.stretchEnd(), {once: true});
+  }
+
+  stretchEnd(): void {
+    this.stretching = false;
+    this.cdr.detectChanges();
+    if (this.mousemoveEventListener) {
+      document.removeEventListener('mousemove', this.mousemoveEventListener);
+    }
+  }
+
+  handleStretchTopEvent(event: MouseEvent): void {
+    const snapTo: number = this.getSnapTo(event);
+    const oldStartTimeMinutes: number = this.start.getHours() * 60 + this.start.getMinutes();
+    const endTimeMinutes: number = oldStartTimeMinutes + this.durationMinutes;
+    let newStartTimeMinutes: number = this.getPointerTopOffsetMinutes(event, snapTo);
+    if (oldStartTimeMinutes !== newStartTimeMinutes) {
+      if (newStartTimeMinutes < 0) { // Prevent stretching above the upper bound of the grid
+        newStartTimeMinutes = 0;
+      } else if (newStartTimeMinutes >= endTimeMinutes) { // Prevent stretching below the lower bound of the work log entry
+        newStartTimeMinutes = Math.floor((endTimeMinutes - 1) / snapTo) * snapTo;
+      }
+      this.start.setHours(0, newStartTimeMinutes, 0, 0);
+      this.durationMinutes += oldStartTimeMinutes - newStartTimeMinutes;
+
+      this.computeSizeAndOffset();
+      this.cdr.detectChanges();
+    }
+  }
+
+  handleStretchBottomEvent(event: MouseEvent): void {
+    const snapTo: number = this.getSnapTo(event);
+    const startTimeMinutes: number = this.start.getHours() * 60 + this.start.getMinutes();
+    const oldEndTimeMinutes: number = startTimeMinutes + this.durationMinutes;
+    let newEndTimeMinutes: number = this.getPointerTopOffsetMinutes(event, snapTo);
+    if (oldEndTimeMinutes !== newEndTimeMinutes) {
+      if (newEndTimeMinutes <= startTimeMinutes) { // Prevent stretching above the upper bound of the work log entry
+        newEndTimeMinutes = Math.ceil((startTimeMinutes + 1) / snapTo) * snapTo;
+      } else if (newEndTimeMinutes > 1440) { // Prevent stretching below the grid
+        newEndTimeMinutes = 1440;
+      }
+      this.durationMinutes = newEndTimeMinutes - startTimeMinutes;
+
+      this.computeSizeAndOffset();
+      this.cdr.detectChanges();
+    }
+  }
+
+  getSnapTo(event: MouseEvent): number {
+    if (event.altKey && !event.ctrlKey && !event.shiftKey) {
+      return 1;
+    } else if (!event.altKey && event.ctrlKey && !event.shiftKey) {
+      return 60;
+    } else if (!event.altKey && !event.ctrlKey && event.shiftKey) {
+      return 15;
+    } else {
+      return 5;
+    }
+  }
+
+  getPointerTopOffsetMinutes(event: MouseEvent, snapTo: number): number {
+    return Math.round(
+      (event.clientY + this.gridElement.scrollTop - WorklogEditorComponent.GRID_OFFSET_TOP - this.mouseEventYOffset)
+      / this.pixelsPerMinute / snapTo
+    ) * snapTo;
   }
 }
