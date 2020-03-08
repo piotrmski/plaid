37c5a7707f7fc0d7ccf0725eb53d294ac84d50fa
Piotr Marczyński <piotrmski@gmail.com>
2020-03-08 22:47:06 +0100

    Zoom change easing

--- src/plaid/components/zoom-controls/zoom-controls.component.ts
+++ src/plaid/components/zoom-controls/zoom-controls.component.ts
@@ -1,7 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 /**
- * Dumb component, presents two buttons, handles zoom change shortcuts, and delegates change in pixelsPerMinute value.
+ * Dumb component, presents two buttons, handles zoom change shortcuts, zoom level easing, and delegates change in
+ * pixelsPerMinute value.
  */
 @Component({
   selector: 'plaid-zoom-controls',
@@ -15,6 +16,10 @@ export class ZoomControlsComponent implements OnInit {
   zoomOutButtonActive = false;
   _pixelsPerMinuteBase: number;
 
+  animatedPixelsPerMinuteBase: number;
+  animatedPixelsPerMinuteBaseRateOfChange = 0;
+  requestAnimationFrameHandle: number;
+
   @Input()
   shortcutsDisabled = false;
 
@@ -22,7 +27,7 @@ export class ZoomControlsComponent implements OnInit {
   pixelsPerMinuteChange = new EventEmitter<number>();
 
   ngOnInit(): void {
-    this.pixelsPerMinuteBase = 5 / 4;
+    this.pixelsPerMinuteBase = 1.25;
     // Singleton component, no need to unbind events
     addEventListener('wheel', (e: WheelEvent) => { // Ctrl mouse wheel, Ctrl two finger swipe, pinch
       if (!this.shortcutsDisabled && e.ctrlKey) {
@@ -56,10 +61,62 @@ export class ZoomControlsComponent implements OnInit {
       val = ZoomControlsComponent.MAX_PIXELS_PER_MINUTE_BASE;
     }
 
-    this._pixelsPerMinuteBase = val;
+    if (this._pixelsPerMinuteBase == null) { // If this setter is first called, don't animate.
+      this._pixelsPerMinuteBase = val;
+      this.animatedPixelsPerMinuteBase = val;
+      // Emitted value has reduced binary and decimal precision not to brake layout.
+      this.pixelsPerMinuteChange.emit(Math.round(this._pixelsPerMinuteBase * this._pixelsPerMinuteBase * 128) / 128);
+    } else if (this._pixelsPerMinuteBase !== val) { // If this is a subsequent change of pixelsPerMinuteBase
+      this._pixelsPerMinuteBase = val;
+      const change = this.animatedPixelsPerMinuteBase - val;
+      if (Math.abs(change) < 0.05) { // If change was minimal (less than 0.05), emit final value immediately.
+        this.animatedPixelsPerMinuteBase = val;
+        this.animatedPixelsPerMinuteBaseRateOfChange = 0;
+        this.pixelsPerMinuteChange.emit(Math.round(this._pixelsPerMinuteBase * this._pixelsPerMinuteBase * 128) / 128);
+      } else {
+        // If change is over 0.15, animate change over 100 ms. Otherwise interpolate the animation duration linearly.
+        const animationDuration: number = Math.abs(change) > 0.15 ? 100 : (Math.abs(change) * 1000 - 50);
+        const animationStartTime: number = new Date().getTime();
+        // Parameters A and B of 1-dimensional cubic bezier curve equation.
+        const A: number = this.animatedPixelsPerMinuteBase;
+        const B: number = this.animatedPixelsPerMinuteBase + this.animatedPixelsPerMinuteBaseRateOfChange * animationDuration / 3;
+        if (this.requestAnimationFrameHandle != null) {
+          cancelAnimationFrame(this.requestAnimationFrameHandle);
+        }
+        this.requestAnimationFrameHandle = requestAnimationFrame(
+          () => this.animatePixelsPerMinuteBase(animationStartTime, animationDuration, A, B)
+        );
+      }
+    }
+  }
 
-    // Emit a value with reasonably reduced binary and decimal precision
-    this.pixelsPerMinuteChange.emit(Math.round(this._pixelsPerMinuteBase * this._pixelsPerMinuteBase * 128) / 128);
+  animatePixelsPerMinuteBase(
+    animationStartTime: number, animationDuration: number, A: number, B: number, lastExecutionTime: number = null
+  ): void {
+    const now = new Date().getTime();
+    if (now >= animationStartTime + animationDuration) { // If animation reached the end
+      this.animatedPixelsPerMinuteBaseRateOfChange = 0;
+      this.animatedPixelsPerMinuteBase = this._pixelsPerMinuteBase;
+      this.requestAnimationFrameHandle = null;
+      // Emitted value has reduced binary and decimal precision not to brake layout.
+      this.pixelsPerMinuteChange.emit(Math.round(this._pixelsPerMinuteBase * this._pixelsPerMinuteBase * 128) / 128);
+    } else { // If animation is in progress
+      // Animation progress, from 0 to 1.
+      const x: number = (now - animationStartTime) / animationDuration;
+      // Animated ppm base value calculated using 1-dimensional cubic bezier curve equation. Parameters C and D are
+      // both equal to this._pixelsPerMinuteBase.
+      const newAnimatedPixelsPerMinuteBase: number = A + (B - A) * 3 * x * (1 - x) * (1 - x) +
+        (this._pixelsPerMinuteBase - A) * x * x * (3 - 2 * x);
+      if (lastExecutionTime != null && now !== lastExecutionTime) {
+        this.animatedPixelsPerMinuteBaseRateOfChange =
+          (newAnimatedPixelsPerMinuteBase - this.animatedPixelsPerMinuteBase) / (now - lastExecutionTime);
+      }
+      this.animatedPixelsPerMinuteBase = newAnimatedPixelsPerMinuteBase;
+      this.pixelsPerMinuteChange.emit(Math.round(this.animatedPixelsPerMinuteBase * this.animatedPixelsPerMinuteBase * 128) / 128);
+      this.requestAnimationFrameHandle = requestAnimationFrame(
+        () => this.animatePixelsPerMinuteBase(animationStartTime, animationDuration, A, B, now)
+      );
+    }
   }
 
   zoomIn(): void {
